0e90d7381511430be02db7b262758735863307d4
Philip Tantsidis <ptantsidis@gmail.com>
2021-11-16 09:56:23 -0500

    deploy

--- public/db.js
+++ public/db.js
@@ -56,8 +56,6 @@ function checkDatabase() {
 
             // Assign the current store to a variable
             const currentStore = transaction.objectStore('BudgetStore');
-
-            // Clear existing entries because our bulk add was successful
             currentStore.clear();
             console.log('Clearing store 🧹');
           }
